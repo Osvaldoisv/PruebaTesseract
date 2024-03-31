748c47dbab71e98206e28eaa2cd0cee20fc4925d
Osvaldo Sepulveda <osvaldo.sepulveda.v@ug.uchile.cl>
2024-03-31 20:12:33 -0300

    pytesseract funcionando

--- prueba_tesseract.py
+++ prueba_tesseract.py
@@ -1,12 +1,12 @@
 import cv2
 import pytesseract
 
-# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
 image = cv2.imread('imgColores.jpg')
 
-text = pytesseract.image_to_string(image,lang='spa')
-print('Texto: ',text)
+text = pytesseract.image_to_string(image,lang='eng')
+print('Text: ',text)
       
 cv2.imshow('Image',image)
 cv2.waitKey(0)
